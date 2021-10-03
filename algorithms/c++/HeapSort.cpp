fb917b9c97f4a0a4bc57f21a58064c4e899baf69
Anuj Singh Kushwaha <91312210+whyanujjwhy@users.noreply.github.com>
2021-10-03 10:11:32 +0530

    Update HeapSort.cpp

--- algorithms/c++/HeapSort.cpp
+++ algorithms/c++/HeapSort.cpp
@@ -1,6 +1,12 @@
 /*  
+Heap Sort is based on the binary heap data structure. In the binary heap the child nodes of a parent node 
+are smaller than or equal to it in the case of a max heap, and the child nodes of a parent node are 
+greater than or equal to it in the case of a min heap.
 
 
+This is a max heap sort algorithm!
+
+author: @whyanujjwhy
 
 */
 
@@ -52,11 +58,11 @@ void display(int arr[], int n) {
 //Driver code
 int main() {
   int n;
-  cout<<"Enter size of array: ";
+  cout<<"Enter size of array: ";      //user input for size of array
   cin>>n;
   int arr[n];
   cout<<"\nEnter space-separated "<<n<<" elements for sorting: ";
-  for(int i=0; i<n; i++)
+  for(int i=0; i<n; i++)             //loop for arrat element input  [we can also initialise the array] 
     cin>>arr[i];
   cout<<"\nOriginal array:\n";
   display(arr, n);
