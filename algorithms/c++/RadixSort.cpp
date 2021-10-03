afc338d00312f9f0fd3f7c5cc927894e55ad8cb6
Anuj Singh Kushwaha <91312210+whyanujjwhy@users.noreply.github.com>
2021-10-03 13:01:54 +0530

    Update RadixSort.cpp

--- algorithms/c++/RadixSort.cpp
+++ algorithms/c++/RadixSort.cpp
@@ -1,6 +1,5 @@
 /*  
 Radix sort is a sorting algorithm that sorts the elements by first grouping the individual digits of the same place value. 
-Then, sort the elements according to their increasing/decreasing order.
 
 Contributor: @whyanujjwhy
 */
