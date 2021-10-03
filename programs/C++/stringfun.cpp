796d27365c4f690674a7c04609f37fa99f2c3715
2605nidhi <74005670+2605nidhi@users.noreply.github.com>
2021-10-03 10:38:09 +0530

    Added string function file

---
+++ programs/C++/stringfun.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main()
+{
+	string s1 ="fam";
+	string s2 ="ily";
+	//append
+	//s1.append(s2);
+	cout<< s1 + s2 <<endl; 
+	
+	string abc ="nidhi is a good girl";
+	abc.clear();
+    cout<<abc<<endl;	
+	
+	return 0;
+}
